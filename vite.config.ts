4a00b4f5ccb5ffcace999b6fa63a876ed4c5178a
zeejfps <zeejfps@gmail.com>
2026-03-31 23:46:21 -0500

    Added workflow to publish to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/utilities_app/',
   plugins: [svelte(), tailwindcss()],
   server: {
     proxy: {
